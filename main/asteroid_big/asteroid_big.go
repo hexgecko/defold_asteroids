502e08f1ef547d149a2d681a6e952aa83b57a963
info <info@hexgecko.com>
2021-01-16 22:31:50 +0100

    Added a big asteroid.

--- main/asteroid_big/asteroid_big.go
+++ main/asteroid_big/asteroid_big.go
@@ -1,6 +1,6 @@
 components {
   id: "movement"
-  component: "/main/small_asteroid/movement.script"
+  component: "/main/asteroid_big/movement.script"
   position {
     x: 0.0
     y: 0.0
@@ -28,11 +28,26 @@ components {
     w: 1.0
   }
 }
+components {
+  id: "explotion"
+  component: "/main/asteroid_big/explotion.particlefx"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
 embedded_components {
   id: "sprite"
   type: "sprite"
   data: "tile_set: \"/main/main.atlas\"\n"
-  "default_animation: \"asteroid_small\"\n"
+  "default_animation: \"asteroid_big\"\n"
   "material: \"/builtins/materials/sprite.material\"\n"
   "blend_mode: BLEND_MODE_ALPHA\n"
   ""
@@ -63,8 +78,8 @@ embedded_components {
   "  shapes {\n"
   "    shape_type: TYPE_SPHERE\n"
   "    position {\n"
-  "      x: 0.0\n"
-  "      y: 0.0\n"
+  "      x: -4.0\n"
+  "      y: 8.0\n"
   "      z: 0.0\n"
   "    }\n"
   "    rotation {\n"
@@ -76,7 +91,24 @@ embedded_components {
   "    index: 0\n"
   "    count: 1\n"
   "  }\n"
-  "  data: 20.0\n"
+  "  shapes {\n"
+  "    shape_type: TYPE_SPHERE\n"
+  "    position {\n"
+  "      x: 2.0\n"
+  "      y: -10.0\n"
+  "      z: 0.0\n"
+  "    }\n"
+  "    rotation {\n"
+  "      x: 0.0\n"
+  "      y: 0.0\n"
+  "      z: 0.0\n"
+  "      w: 1.0\n"
+  "    }\n"
+  "    index: 1\n"
+  "    count: 1\n"
+  "  }\n"
+  "  data: 32.941\n"
+  "  data: 32.941\n"
   "}\n"
   "linear_damping: 0.0\n"
   "angular_damping: 0.0\n"
@@ -95,9 +127,27 @@ embedded_components {
   }
 }
 embedded_components {
+  id: "small_asteriod_factory"
+  type: "factory"
+  data: "prototype: \"/main/asteroid_small/asteroid_small.go\"\n"
+  "load_dynamically: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
   id: "decoy_factory"
   type: "factory"
-  data: "prototype: \"/main/small_asteroid/decoy.go\"\n"
+  data: "prototype: \"/main/asteroid_big/decoy.go\"\n"
   "load_dynamically: false\n"
   ""
   position {
