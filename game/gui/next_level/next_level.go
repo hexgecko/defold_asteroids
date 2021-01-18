97c96277841cb506a70b4e584b7a69b23acaa0f7
info <info@hexgecko.com>
2021-01-18 14:56:08 +0100

    Added GUI and navigation for two levels.

---
+++ game/gui/next_level/next_level.go
@@ -0,0 +1,30 @@
+components {
+  id: "next_level"
+  component: "/game/gui/next_level/next_level.gui"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "script"
+  component: "/game/gui/next_level/next_level.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
---
+++ game/gui/restart/restart.go
@@ -0,0 +1,30 @@
+components {
+  id: "restart"
+  component: "/game/gui/restart/restart.gui"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "script"
+  component: "/game/gui/restart/restart.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
